347c7f17add2105ae150efdda231b6ea93897da7
William Lowrimore <wlowrimore@gmail.com>
2023-03-16 18:14:55 -0500

    basic setup, routes and pages

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import NewPlace from "./places/pages/NewPlace";
+
+import Users from "./users/pages/Users";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
